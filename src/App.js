0084fa3f342b6b5110bd3b102f576a1308cc2f8c
alexgordontlv <alexaybn@gmail.com>
2021-05-02 14:44:02 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import Sidebar from './components/sidebar/Sidebar';
+import Dashboard from './components/dashboard/Dashboard';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [scores, setScores] = useState([]);
+	useEffect(() => {
+		console.log(scores);
+	}, [scores]);
+
+	return (
+		<div className='App'>
+			<Sidebar scores={scores} setScores={setScores} />
+			<Dashboard scores={scores} />
+		</div>
+	);
 }
 
 export default App;
